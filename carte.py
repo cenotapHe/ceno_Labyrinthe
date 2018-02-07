bc470a7f4cc5a9fd46efedba012930591c2ce444
cenotapHe <bat1ste@hotmail.com>
2018-02-07 10:36:49 +0100

    Premiere ebauche du Labyrinthe

---
+++ carte.py
@@ -0,0 +1,234 @@
+# -*-coding:Utf-8 -*
+
+import os
+
+"""Ce module contient la classe Carte."""
+
+class Cartes:
+
+    """Objet de transition entre un fichier et un labyrinthe."""
+
+    def __init__(self, base = {}, **couple_nom_labyrinthe):
+
+        self._noms_des_cartes = []
+        self._labyrinthes_des_cartes = []
+
+
+class Carte_en_cours():
+
+	def __init__(self, _plateau_de_jeu):
+
+		self._plateau_de_jeu = _plateau_de_jeu
+
+
+	def __repr__(self):
+
+		os.system("cls")
+		chaine = "X = Robot\nU = Sortie\n. = Porte\nO = Mur\n\n" + self._plateau_de_jeu
+		return chaine
+
+	def __str__(self):
+
+		return __repr__(self)
+
+
+		
+
+	def deplacement_droite(self, nombre):
+
+		mouvement = nombre
+		pas = 0
+
+		while nombre > 0 :
+			i = 0
+			nombre -= 1
+			while i < len(self._plateau_de_jeu) :
+				i += 1
+				try:
+					if self._plateau_de_jeu[i] == str('X'):
+						if self._plateau_de_jeu[i + 1] == str('O'):
+							print("On ne peut pas traverser les murs !")
+							nombre = 0
+							continue
+						else:
+							self._plateau_de_jeu = self._plateau_de_jeu[:i] + " " + self._plateau_de_jeu[(i + 1):]
+							self._plateau_de_jeu = self._plateau_de_jeu[:(i + 1)] + "X" + self._plateau_de_jeu[(i + 2):]
+							pas += 1
+							break
+					
+				except IndexError:
+					pass
+
+		input("Vous vous êtes déplacez de {} pas vers l'Est.".format(pas))
+
+	def deplacement_gauche(self, nombre):
+
+		mouvement = nombre
+		pas = 0
+
+		while nombre > 0 :
+			i = len(self._plateau_de_jeu)
+			nombre -= 1
+			while i > 0 :
+				i -= 1
+				try:
+					if self._plateau_de_jeu[i] == str('X'):
+						
+						#if self._plateau_de_jeu[i - 1] == str('U'):
+						#	print("Vous venez de vous échapper du labyrinthe !!!")
+
+						if self._plateau_de_jeu[i - 1] == str('O'):
+							print("On ne peut pas traverser les murs !")
+							nombre = 0
+							continue
+						else:
+							self._plateau_de_jeu = self._plateau_de_jeu[:i] + " " + self._plateau_de_jeu[(i + 1):]
+							self._plateau_de_jeu = self._plateau_de_jeu[:(i - 1)] + "X" + self._plateau_de_jeu[i:]
+							pas += 1
+							break
+					
+				except IndexError:
+					pass
+
+		input("Vous vous êtes déplacez de {} pas vers l'Ouest.".format(pas))
+
+	def largeur_plateau(self):
+
+		largeur = 2
+		i = 1
+		boucle = True
+		while boucle == True :
+			if self._plateau_de_jeu[i] == self._plateau_de_jeu[i + 1]:
+				largeur += 1
+				i +=1
+			else:
+				boucle = False
+
+		return largeur
+
+
+	def deplacement_bas(self, nombre):
+
+		mouvement = nombre
+		pas = 0
+
+		j = self.largeur_plateau()
+
+		while nombre > 0 :
+			i = 0
+			nombre -= 1
+			while i < len(self._plateau_de_jeu) :
+				i += 1
+				try:
+					if self._plateau_de_jeu[i] == str('X'):
+						if self._plateau_de_jeu[i + j + 1] == str('O'):
+							print("On ne peut pas traverser les murs !")
+							nombre = 0
+							continue
+						else:
+							self._plateau_de_jeu = self._plateau_de_jeu[:i] + " " + self._plateau_de_jeu[(i + 1):]
+							self._plateau_de_jeu = self._plateau_de_jeu[:(i + j + 1)] + "X" + self._plateau_de_jeu[(i + j + 2):]
+							pas += 1
+							break
+					
+				except IndexError:
+					pass
+
+		input("Vous vous êtes déplacez de {} pas vers le Sud.".format(pas))
+
+	def deplacement_haut(self, nombre):
+
+		mouvement = nombre
+		pas = 0
+
+		j = self.largeur_plateau()
+
+		while nombre > 0 :
+			
+			i = 0
+			nombre -= 1
+			while i < len(self._plateau_de_jeu) :
+
+				i += 1
+				try:
+					if self._plateau_de_jeu[i] == str('X'):
+						if self._plateau_de_jeu[i - j - 1] == str('O'):
+							print("On ne peut pas traverser les murs !")
+							nombre = 0
+							continue
+						else:
+							self._plateau_de_jeu = self._plateau_de_jeu[:i] + " " + self._plateau_de_jeu[(i + 1):]
+							self._plateau_de_jeu = self._plateau_de_jeu[:(i - j - 1)] + "X" + self._plateau_de_jeu[(i - j):]
+							pas += 1
+							break
+					
+				except IndexError:
+					pass
+
+		input("Vous vous êtes déplacez de {} pas vers le Nord.".format(pas))
+
+	def partie_en_cours(self):
+
+		jeu_en_cours = True
+		premier_cycle = True
+
+		while jeu_en_cours == True :
+
+			os.system("cls")
+			chaine = "X = Robot\nU = Sortie\n. = Porte\nO = Mur\n\n" + self._plateau_de_jeu
+			print(chaine)
+
+			if premier_cycle == True :
+				commande = input("\n     Commande de jeu :\n\n     N              Nord\n  O     E        Ouest/Est\n     S              Sud\n\nQ: sauvegarder et Quitter\n\nAstuce: vous pouvez indiquer le nombre \nde pas à coté de la direction.\nexemple: \"S3\" vous fera vous déplacer \nde 3 pas vers le Sud.\n\n Commande :  ")
+				premier_cycle = False
+			else :
+				commande = input("\n     Commande de jeu :\n\n     N              Nord\n  O     E        Ouest/Est\n     S              Sud\n\nQ: sauvegarder et Quitter\n\n Commande :  ")
+
+			if len(commande) > 2 or len(commande) <= 0 :
+				input("Veuillez saisir une lettre correspondant à une des actions.\nAvec, optionnellement un seul et unique chiffre.")
+				continue
+
+			try :
+				commande = int(commande)
+				input("Veuillez saisir une commande, et pas uniquement des chiffres.")
+				continue
+			except:
+				pass
+
+			commande = commande.capitalize()
+			
+			try :
+				lettre = str(commande[0])
+			except :
+				input("Commande non valide.")
+				continue
+
+			try:
+				nombre = int(commande[1])
+			except IndexError :
+				nombre = 1
+			except ValueError :
+				input("Uniquement une seule lettre et un seul chiffre !!")
+				continue
+			except UnboundLocalError :
+				input("Merci de taper une seule commande, et une unique fois.")
+				continue
+
+			if lettre == "S":
+				self.deplacement_bas(nombre)
+			elif lettre == "N":
+				self.deplacement_haut(nombre)
+			elif lettre == "O":
+				self.deplacement_gauche(nombre)
+			elif lettre == "E":
+				self.deplacement_droite(nombre)
+			elif lettre == "Q":
+				input("Quit")
+			else :
+				input("Je suis désolé, je n'ai pas compris votre demande.")
+				
+
+
+
+#    def __repr__(self):
+#        return "<Carte {}>".format(self.nom)
---
+++ roboc.py
@@ -0,0 +1,80 @@
+# -*-coding:Utf-8 -*
+
+"""Ce fichier contient le code principal du jeu.
+
+Exécutez-le avec Python pour lancer le jeu.
+
+"""
+
+import os
+
+from carte import *
+
+
+
+# On charge les cartes existantes
+diffrentes_cartes_de_jeu = Cartes()
+
+for nom_fichier in os.listdir("cartes"):
+    if nom_fichier.endswith(".txt"):
+        chemin = os.path.join("cartes", nom_fichier)
+        nom_carte = nom_fichier[:-4].capitalize()
+        with open(chemin, "r") as fichier:
+            contenu = fichier.read()
+
+            #print(nom_carte)
+            #print(contenu)
+
+            diffrentes_cartes_de_jeu._noms_des_cartes.append(nom_carte)
+            diffrentes_cartes_de_jeu._labyrinthes_des_cartes.append(contenu) # on l'imprime sous la forme print(diffrentes_cartes_de_jeu._labyrinthe[i])
+
+
+            
+            # Création d'une carte, à compléter
+
+
+
+# On affiche les cartes existantes
+print("Labyrinthes existants :\n")
+for i, carte in enumerate(diffrentes_cartes_de_jeu._noms_des_cartes):
+    print("  {} - {}".format(i + 1, carte))
+
+
+# On fait une boucle pour que le joueur choisise son labyrinthe
+i = True
+while i == True:
+	choix_du_labyrinthe = input("\nQuelle labyrinthe choisisez-vous ? ")
+	try :
+		choix_du_labyrinthe = int(choix_du_labyrinthe)
+		if choix_du_labyrinthe <= 0 or choix_du_labyrinthe > len(diffrentes_cartes_de_jeu._noms_des_cartes):
+			print("Ce numéro de labyrtinhe n'existe pas !")
+			continue
+		i = False
+		input("Vous avez choisit le labyrinthe {}, qui se nomme \"{}\"".format(choix_du_labyrinthe, diffrentes_cartes_de_jeu._noms_des_cartes[choix_du_labyrinthe - 1]))
+	except ValueError:
+		print("On vous a demandé de choisir le numéro du labyrinthe.")
+
+
+carte_du_jeu_en_cours = Carte_en_cours(diffrentes_cartes_de_jeu._labyrinthes_des_cartes[choix_du_labyrinthe - 1])
+
+carte_du_jeu_en_cours.partie_en_cours()
+
+
+
+
+
+
+#carte_du_jeu_en_cours.deplacement_droite(4)
+
+
+#mouvement = input("On va allez à droite, alors appuyer sur D")
+#if mouvement == "d":
+
+	
+os.system("pause")
+
+
+
+# Si il y a une partie sauvegardée, on l'affiche, à compléter
+
+# ... Complétez le programme ...
